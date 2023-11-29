6609413bb2371d23a36da5b79590451a657555cb
Jiří Hlaváček <Hlavacek.Jiri@vassboskovice.cz>
2023-11-29 13:25:43 +0100

    Přidejte soubory projektu.

---
+++ Fireworks_IT3/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Fireworks_IT3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Rocket> rockets = new List<Rocket>();
+        DispatcherTimer timer = new DispatcherTimer();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 33);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void DrawRockets()
+        {
+            CanvasSky.Children.Clear();
+            foreach(var rocket in rockets)
+            {
+                Ellipse ellipse = new Ellipse();
+                ellipse.Width = 10;
+                ellipse.Height = 10;
+                ellipse.Fill = Brushes.White;
+                Canvas.SetLeft(ellipse, rocket.Location.X);
+                Canvas.SetTop(ellipse, rocket.Location.Y);
+                CanvasSky.Children.Add(ellipse);
+            }
+        }
+
+        private void Timer_Tick(object? sender, EventArgs e)
+        {
+            foreach (var rocket in rockets)
+            {
+                rocket.Move();
+            }
+            DrawRockets();
+        }
+
+        private void CanvasSky_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Rocket rocket = new Rocket(new Point(100, 200), new Vector(2, -10), 2);
+            rocket.Exploded += Rocket_Exploded;
+            rockets.Add(rocket);
+        }
+
+        private void Rocket_Exploded(List<Rocket> obj)
+        {
+            rockets.AddRange(obj);
+        }
+
+
+    }
+}
---
+++ Fireworks_IT3/Rocket.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Fireworks_IT3
+{
+    public class Rocket
+    {
+        private Vector gravity { get; } = new Vector(0, 0.3);
+        public Point Location { get; set; }
+        public Vector SpeedVector { get; set; }
+        public int Level { get; set; }
+        private int countdown = 60;
+
+        public event Action<List<Rocket>>? Exploded;
+
+        public Rocket(Point location, Vector speedVector, int level)
+        {
+            Location = location;
+            SpeedVector = speedVector;
+            Level = level;
+        }
+
+        public void Move()
+        {
+            Location.Offset(SpeedVector.X, SpeedVector.Y);
+            SpeedVector = Vector.Add(SpeedVector, gravity);
+            countdown--;
+            if(countdown < 0)
+            {
+                Exploded?.Invoke(Explode());
+            }
+        }
+
+        public List<Rocket> Explode()
+        {
+            List<Rocket> rockets = new List<Rocket>();
+            if (Level > 0)
+            {
+                int count = 10;
+                int angle = 360 / count;
+                for (int i = 0; i < 10; i++)
+                {
+                    Vector v = new Vector(Math.Cos(i * angle), Math.Sin(i * angle));
+                    Rocket rocket = new Rocket(Location, v, Level - 1);
+                }
+            }
+            return rockets;
+        }
+    }
+}
